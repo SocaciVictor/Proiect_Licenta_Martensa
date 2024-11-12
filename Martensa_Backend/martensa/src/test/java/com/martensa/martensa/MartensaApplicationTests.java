ab7e90a93b5ed5a402464399dfac07ffa9a135c4
SocaciVictor <147531728+SocaciVictor@users.noreply.github.com>
2024-11-12 19:17:56 +0200

    Initialize structure for backend

---
+++ Martensa_Backend/martensa/src/test/java/com/martensa/martensa/MartensaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martensa.martensa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MartensaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
