ff63dab9c0f4f0b5066eb64ba08e963146c6f120
SocaciVictor <147531728+SocaciVictor@users.noreply.github.com>
2024-11-12 20:50:53 +0200

    Created all model for database

---
+++ Martensa_Backend/martensa/src/main/java/com/martensa/martensa/model/Order.java
@@ -0,0 +1,53 @@
+package com.martensa.martensa.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "ORDERS")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private LocalDate orderDate;
+    private BigDecimal totalAmount;
+    private String orderStatus;
+    private String shippingAddress;
+    private String paymentMethod;
+    private String trackingNumber;
+
+    @ManyToMany
+    @JoinTable(
+        name = "order_product",
+        joinColumns = @JoinColumn(name = "order_id"),
+        inverseJoinColumns = @JoinColumn(name = "product_id")
+    )
+    private List<Product> products;
+}
---
+++ Martensa_Backend/martensa/src/main/java/com/martensa/martensa/model/enums/UserRole.java
@@ -0,0 +1,7 @@
+package com.martensa.martensa.model.enums;
+
+public enum UserRole {
+    ADMIN,
+    CUSTOMER,
+    GUEST
+}
